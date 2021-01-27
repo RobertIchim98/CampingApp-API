dfc8d883e8950d1159cc11ce11ea2dac14ef230c
Robert <robertichim98@gmail.com>
2021-01-27 19:22:08 +0000

    Add postGIS database in settings

--- api/settings.py
+++ api/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'gis',
+        'USER': 'docker',
+        'PASSWORD': 'docker',
+        'HOST': 'localhost',
     }
 }
 
