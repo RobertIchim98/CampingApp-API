ced9da4a5c8a0236e810b72c90f223db1bb54998
Robert <robertichim98@gmail.com>
2021-03-10 14:29:49 +0000

    have the owner be the username not the ID

--- world/serializers.py
+++ world/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 
 from .models import CampingSpot
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 class CampingSpotSerializer(serializers.ModelSerializer):
+    # use owner username instead of id
+    owner = serializers.SlugRelatedField(slug_field="username", queryset=User.objects.all())
+
+
     class Meta:
         model = CampingSpot
         lat_lon = serializers.Field()
